61fed988abaf11369e50f48252b21b6c9d7120c6
Jacob Hegwood <jacobhegwood80@gmail.com>
2020-11-20 22:20:37 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// This is a program for generating random character lore. The program generates a random message using three parts to form the message every time it is run.
+
+// parts pool
+
+const birthplace = [];
+
+const hobbies = [];
+
+const professions = [];
+
+// set up a pool of messages
+
+console.log("running");
